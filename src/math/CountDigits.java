0f705b89ea7f085a48d3c5da72963b56eb455f74
Sakshi Jaiswal <sakshijaiswal916@gmail.com>
2025-07-27 15:11:15 +0530

    Added math DSA practice files

---
+++ src/math/CountDigits.java
@@ -0,0 +1,17 @@
+package math;
+
+public class CountDigits {
+    static int count(int n){
+        int count = 0;
+        while(n > 0){
+            int lastDigit = n % 10;
+            count++;
+            n = n / 10;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(count(23));
+    }
+}
