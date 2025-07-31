253dfc830c083d0cc36b2eeb99e37966fc2f7403
Sakshi Jaiswal <sakshijaiswal916@gmail.com>
2025-07-31 16:55:32 +0530

    Solved Bubble Sort Algorithm

---
+++ src/SortingAlgorithms/BubbleSort.java
@@ -0,0 +1,52 @@
+package SortingAlgorithms;
+
+public class BubbleSort {
+    public static void bubbleSort(int[] arr){
+        int n = arr.length-1;
+        for(int i = 0; i < n ; i++){
+            for(int j = 0; j < n-i ;j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12, 4 , 7 , 6 , 1  , 3};
+        bubbleSort(arr);
+        for(int value : arr){
+            System.out.print(value + " ");
+        }
+    }
+}
+
+
+// OPTIMISED BUBBLE SORT IN CASE OF NEARLY SORTED ARRAYS
+/* public class BubbleSort {
+    public static void bubbleSort(int[] arr){
+        int n = arr.length-1;
+        for(int i = 0; i < n ; i++){
+            boolean flag = false;
+            for(int j = 0; j < n-i ;j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    flag = true;
+                }
+            }
+            if(!flag) return;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3,1,4,5,6};
+        bubbleSort(arr);
+        for(int value : arr){
+            System.out.print(value + " ");
+        }
+    }
+}*/
